786d51fac8b39962fdd8185fc2188606e9a4e105
Matt DeKrey <mattdekrey@gmail.com>
2024-08-08 18:22:26 -0500

    Add typechecked rules

--- packages/eslint-config/src/untyped.ts
+++ packages/eslint-config/src/untyped.ts
@@ -1,17 +1,10 @@
+import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import type { TSESLint } from "@typescript-eslint/utils";
 import eslintConfigPrettier from "eslint-config-prettier";
-import untyped from "./untyped";
 
 export default tseslint.config(
-	...untyped,
-	...tseslint.configs.recommendedTypeChecked,
+	eslint.configs.recommended,
+	...tseslint.configs.recommended,
 	eslintConfigPrettier,
-	{
-		languageOptions: {
-			parserOptions: {
-				projectService: true,
-			},
-		},
-	},
 ) as TSESLint.FlatConfig.ConfigArray;
