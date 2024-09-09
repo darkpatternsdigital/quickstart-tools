3362bce92c2f7519cc0d9ab36896ba151673fb8f
Matt DeKrey <mattdekrey@gmail.com>
2024-09-09 13:37:07 -0500

    Add a PNPM API quickstart project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,4 +6,7 @@ import { tseslint } from '@darkpatternsdigital/eslint-config/types';
 export default tseslint.config(
 	includeIgnoreFile(fileURLToPath(import.meta.resolve('./.gitignore'))),
 	...dpeslint,
+	{
+		ignores: ['Templates/content/**/*'],
+	},
 );
